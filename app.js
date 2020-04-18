0aaaa57a2c7a715298f4681dc22292d42c96be4a
jeetg57 <jeetg57@gmail.com>
2020-04-18 15:29:47 +0300

    New Commit

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use("/images", imageRoute);
 app.use(express.static(__dirname + "/uploads/"));
 
 //connect to db
+
 mongoose.connect(
   process.env.DB_CONNECTION,
   { useNewUrlParser: true, useUnifiedTopology: true },
@@ -27,9 +28,7 @@ if (process.env.NODE_ENV == "production") {
   app.use(express.static("client/build"));
 
   app.get(
-    "*",
-    (req,
-    (res) => {
+    "*",(req,res) => {
       res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     })
   );
